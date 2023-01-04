c8a609f8abb4170a710e6ca1e2e54fba2f12de3b
Nguyen Van Huyen <lockex1987@gmail.com>
2023-01-04 11:00:52 +0700

    Basic validate

--- uw-passport-frontend/src/pages/roles/RolesImport.vue
+++ uw-passport-frontend/src/pages/roles/RolesImport.vue
@@ -11,7 +11,6 @@
 
 
 <script setup>
-import dayjs from 'dayjs'
 import Schema from 'async-validator'
 import ImportModal from '@/components/ImportModal.vue'
 
@@ -21,14 +20,6 @@ const rules = {
 }
 
 const validator = new Schema(rules)
-
-const facebookUrlPattern = 'https?:\\/\\/(www\\.)?(facebook|fb)\\.com\\/\\S+\\/?'
-
-// Biểu thức chính quy Facebook
-const facebookPattern = '^' + facebookUrlPattern + '$'
-
-// Thời gian hiện tại
-const currentDate = dayjs()
 
 const importModal = ref()
 
@@ -46,89 +37,13 @@ const validateRow = async rowData => {
   }
 
   const validateErrors = []
-  validator.validate(data, (errors, fields) => {
-    if (errors) {
-      console.log(errors, fields)
-    }
-
-    // validation passed
-  })
-
-
-
-  /*
-  const actionTimeRules = CV.parseValidationFromString('required')
-  const actionTimeError = CV.validateByValueAndValidation(actionTimeStr, actionTimeRules)
-  if (actionTimeError) {
-    validateErrors.push('Thời gian: ' + actionTimeError)
-  } else {
-    const dateTimeFormat = 'DD/MM/YYYY HH:mm:ss'
-    const actionTimeMoment = moment(actionTimeStr, dateTimeFormat)
-    rowData[col++] = actionTimeMoment
-
-    if (actionTimeMoment.format(dateTimeFormat) != actionTimeStr) {
-      const message = 'Không đúng định dạng ' + dateTimeFormat
-      validateErrors.push('Thời gian: ' + message)
-    } else if (actionTimeMoment.isAfter(this.currentDate)) {
-      const message = 'Không được sau hiện tại'
-      validateErrors.push('Thời gian: ' + message)
-    }
+  try {
+    await validator.validate(data)
+  } catch ({ errors, fields }) {
+    errors.forEach(e => {
+      validateErrors.push(e.message)
+    })
   }
-  */
-
-  /*
-  const wallTypeRules = CV.parseValidationFromString('required')
-  const wallTypeError = CV.validateByValueAndValidation(wallTypeStr, wallTypeRules)
-  let wallTypeId
-  if (wallTypeError) {
-    validateErrors.push('Nơi tương tác: ' + actionTimeError)
-  } else {
-    const placeTypeObj = this.wallTypeList.find(e => e.name.toLowerCase() == wallTypeStr.toLowerCase())
-    if (! placeTypeObj) {
-      validateErrors.push('Nơi tương tác phải là "Trang" hoặc "Nhóm công khai"')
-    } else {
-      rowData[col++] = placeTypeObj.id
-      wallTypeId = placeTypeObj.id
-    }
-  }
-  */
-
-  /*
-  const fbIdRules = CV.parseValidationFromString('required')
-  const fbIdError = CV.validateByValueAndValidation(fbId, fbIdRules)
-  if (fbIdError) {
-    validateErrors.push('Facebook ID của nơi tương tác: ' + fbIdError)
-  } else {
-    if (wallTypeId == 'page') {
-      const pageObj = this.pageList.find(e => e.fbId.toLowerCase() == fbId.toLowerCase())
-      if (! pageObj) {
-        const message = 'Không tồn tại'
-        validateErrors.push('Trang: ' + message)
-      } else {
-        rowData[col++] = pageObj.id
-      }
-    } else if (wallTypeId == 'group') {
-      const groupObj = this.groupList.find(e => e.fbId.toLowerCase() == fbId.toLowerCase())
-      if (! groupObj) {
-        const message = 'Không tồn tại'
-        validateErrors.push('Nhóm công khai: ' + message)
-      } else {
-        rowData[col++] = groupObj.id
-      }
-    }
-  }
-  */
-
-  /*
-  const sourceUrlRules = CV.parseValidationFromString('required|maxLength:512')
-  sourceUrlRules.regexPattern = this.facebookPattern
-  sourceUrlRules.regexPatternMessage = 'Vui lòng nhập URL của Facebook'
-  const sourceUrlError = CV.validateByValueAndValidation(sourceUrl, sourceUrlRules)
-  if (sourceUrlError) {
-    validateErrors.push('Đường dẫn bài đăng: ' + sourceUrlError)
-  }
-  */
-
   return validateErrors
 }
 
@@ -141,7 +56,6 @@ const insertRow = async rowData => {
     name,
     permissions: [],
   }
-
   const response = await axios.post('/api/roles/insert', params)
   return response
 }
